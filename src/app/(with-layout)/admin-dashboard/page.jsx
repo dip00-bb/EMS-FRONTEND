d499a901f500a68d2f10301d0f16e5f1f35089be
Dip908 <dipchondo007@gmail.com>
2025-12-14 19:08:24 +0600

    create sidebar for admin dashboard

--- src/app/(with-layout)/admin-dashboard/page.jsx
+++ src/app/(with-layout)/admin-dashboard/page.jsx
@@ -1,18 +1,13 @@
 
 "use client"
-import useAuth from '@/app/Hooks/useAuth';
-import { axiosPrivate } from '@/app/utils/axiosPrivate';
-import React, { useEffect } from 'react';
+import AdminSidebar from '@/app/Components/dashboard/AdminSidebar';
+import React from 'react';
 
 const AdminDashboard = () => {
-    const { user,isLoading } = useAuth()
-    if(isLoading) {
-        return <div>Loading...</div>
-    }
-    console.log(user)
+
     return (
         <div>
-            Admin Dashboard
+            <AdminSidebar/>
         </div>
     );
 };
---
+++ src/app/Components/dashboard/AdminSidebar.jsx
@@ -0,0 +1,77 @@
+import { Banknote, Building, Calendar, LayoutDashboard, Settings, Users } from 'lucide-react';
+import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import React from 'react';
+
+
+
+
+const AdminSidebar = () => {
+
+    const pathName=usePathname()
+
+    return (
+        <div className='w-64 h-screen top-0 bottom-0 left-0 space-y-2 bg-(--neurtal-color)'>
+            <div className='bg-(--primary-color) py-3 text-center'>
+                <h3 className='text-2xl pacifico-regular text-white'>Employee MS</h3>
+            </div>
+            <div className='px-4'>
+                <div>
+                    <Link href={'/admin-dashboard'}
+                        className={ ` ${pathName==='/admin-dashboard' ? "bg-red-500 rounded animate-pulse":"" } flex items-center gap-3 py-2.5 px-4`}
+                    >
+                        <LayoutDashboard color='white' />
+                        <span>Dashboard</span>
+                    </Link>
+                </div>
+                <div>
+                    <Link href={'/'}
+                        className='flex items-center gap-3 py-2.5 px-4'
+                    >
+                        <Users color='white' />
+                        <span>Employee</span>
+                    </Link>
+                </div>
+                <div>
+                    <Link href={'/'}
+                        className='flex items-center gap-3 py-2.5 px-4'
+                    >
+                        <Building color='white' />
+                        <span>Department</span>
+                    </Link>
+                </div>
+
+                <div>
+                    <Link href={'/'}
+                        className='flex items-center gap-3 py-2.5 px-4'
+                    >
+                        <Calendar color='white' />
+                        <span>Leaves</span>
+                    </Link>
+                </div>
+
+
+                <div>
+                    <Link href={'/'}
+                        className='flex items-center gap-3 py-2.5 px-4'
+                    >
+                        <Banknote  color='white'/>
+                        <span>Leaves</span>
+                    </Link>
+                </div>
+
+
+                <div>
+                    <Link href={'/'}
+                        className='flex items-center gap-3 py-2.5 px-4'
+                    >
+                        <Settings color='white' />
+                        <span>Leaves</span>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default AdminSidebar;
