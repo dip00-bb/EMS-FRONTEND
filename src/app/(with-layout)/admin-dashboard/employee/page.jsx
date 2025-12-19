4249cbeab70e8ca4a176c5c1f8f4e90f2a5b523e
Dip908 <dipchondo007@gmail.com>
2025-12-19 18:44:44 +0600

    fetch department list with tanstack query

--- src/app/(with-layout)/admin-dashboard/employee/page.jsx
+++ src/app/(with-layout)/admin-dashboard/employee/page.jsx
@@ -1,9 +1,27 @@
+"use client"
+
+import { axiosPrivate } from '@/app/utils/axiosPrivate';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const Employee = () => {
+
+    const { isPending, error, data } = useQuery({
+        queryKey: ["listOfDepartment"],
+        queryFn: async () => {
+            const response = await axiosPrivate.get('/api/department/get-department');
+
+            console.log(response)
+            if (response?.data?.success) {
+                return response?.data?.departmentList
+            }
+        }
+    })
+
+    console.log(data)
     return (
         <div>
-            dddddddd
+            
         </div>
     );
 };
